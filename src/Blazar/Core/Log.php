c3d2d5b39d199d0e39636ff0c13bf1e78c4a64a4
João Henrique <joao_henriquee@outlook.com>
2019-04-28 16:07:24 -0300

    Adiciona log ao core para evitar incompatibilidade

---
+++ src/Blazar/Core/Log.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of Blazar Framework.
+ *
+ * (c) João Henrique <joao_henriquee@outlook.com>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Blazar\Core;
+
+/**
+ * Class Log
+ *
+ * @deprecated Use \Blazar\Component\Log\Log
+ */
+class Log extends \Blazar\Component\Log\Log {
+
+}
